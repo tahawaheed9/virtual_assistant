89777f86d45fa05af6758686e47e63a1a9603a0b
tahawaheed9 <tahawaheed9@gmail.com>
2025-05-16 19:32:45 +0300

    Removed | Unnecessary code bits

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-import 'package:flutter_native_splash/flutter_native_splash.dart';
-
 import 'package:voice_assistant/views/home/home_view.dart';
 import 'package:voice_assistant/utils/constants/text_strings.dart';
 import 'package:voice_assistant/utils/constants/theme/app_theme.dart';
 
 void main() {
-  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
-
-  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+  WidgetsFlutterBinding.ensureInitialized();
 
   // Setting the background for the status bar to transparent
   SystemChrome.setSystemUIOverlayStyle(
@@ -30,8 +26,6 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
